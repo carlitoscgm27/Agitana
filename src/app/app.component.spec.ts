a2e27cf35003551e7aef1eecc5dc6de2274f7e20
carlitoscgm27 <cgarciamadiedo@safareyes.es>
2023-09-21 16:22:28 +0200

    subida imagenes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,27 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-<<<<<<< HEAD
   it(`should have as title 'ProyectoDAW'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('ProyectoDAW');
-=======
-  it(`should have as title 'proyectoDaw'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('proyectoDaw');
->>>>>>> 835298326f378c35bed4c1b717a1640d062e4fa5
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-<<<<<<< HEAD
     expect(compiled.querySelector('.content span')?.textContent).toContain('ProyectoDAW app is running!');
-=======
-    expect(compiled.querySelector('.content span')?.textContent).toContain('proyectoDaw app is running!');
->>>>>>> 835298326f378c35bed4c1b717a1640d062e4fa5
   });
 });
