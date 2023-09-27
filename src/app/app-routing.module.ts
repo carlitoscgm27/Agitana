bf348c9041fd7c3c1673aefe35749d1daee32899
Antonio López <tonidiez17@gmail.com>
2023-09-27 20:10:28 +0200

    Creacion de navbar y de inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+ const routes: Routes = [
+//   { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+//   { path: 'inicio', component:  },
+//   { path: 'acerca-de', component: },
+//   { path: 'contacto', component:  },
+ ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
