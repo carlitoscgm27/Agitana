9ff8101668998798eebe43e7a1491a29e39152b2
Antonio López <tonidiez17@gmail.com>
2023-09-28 16:40:05 +0200

    subida de navbar y cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
 
  const routes: Routes = [
-//   { path: '', redirectTo: '/inicio', pathMatch: 'full' },
-//   { path: 'inicio', component:  },
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+  { path: 'inicio', component: InicioComponent },
 //   { path: 'acerca-de', component: },
 //   { path: 'contacto', component:  },
  ];
