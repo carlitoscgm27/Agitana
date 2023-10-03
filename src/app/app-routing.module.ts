c3163d8da31be01df34bed5fa0e4ffb852cdc835
Antonio López <tonidiez17@gmail.com>
2023-10-03 17:42:55 +0200

    actualización navbar los if´s

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,40 +4,50 @@ import { InicioComponent } from './inicio/inicio.component';
 import { DonarComponent } from './donar/donar.component';
 import { ErrorPageComponent } from './error-page/error-page.component';
 import { UserService } from './service/user.service';
+import { PersonaTipoService } from './service/persona-tipo.service';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/Inicio', pathMatch: 'full' },
+  { path: 'OngSafa', redirectTo: 'OngSafa/Inicio', pathMatch: 'full' },
 
-  { path: 'Inicio', component: InicioComponent },
-  { path: 'Donar', component: DonarComponent },
+  { path: 'OngSafa/Inicio', component: InicioComponent },
+  { path: 'OngSafa/Donar', component: DonarComponent },
   //{ path: 'contacto', component:  },
-  //{ path: 'Login', component:  },
-  //{ path: 'Register', component:  },
+  //{ path: 'OngSafa/Login', component:  },
+  //{ path: 'OngSafa/Register', component:  },
 
   { path: '**', component: ErrorPageComponent },
 ];
 const routesAdmin: Routes = [
-  // { path: '', redirectTo: '/Alta', pathMatch: 'full' },
+  // { path: 'OngSafa', redirectTo: 'OngSafa/Alta', pathMatch: 'full' },
 
-  //{ path: 'Alta', component:  },
-  //{ path: 'Control', component:  },
-  //{ path: 'Donaciones', component:  },
-  //{ path: 'Solicitud', component:  },
-  //{ path: 'Perfil/?id', component:  },
-
-  { path: '**', component: ErrorPageComponent },
-];
-const routesDonanteRecibidor: Routes = [
-  // { path: '', redirectTo: '/InicioDonante', pathMatch: 'full' },
-
-  // { path: 'InicioDonante', component:  },
-  // { path: 'Donar', component: DonarComponent },
-  //{ path: 'Perfil/?id', component:  },
+  //{ path: 'OngSafa/Alta', component:  },
+  //{ path: 'OngSafa/Control', component:  },
+  //{ path: 'OngSafa/Donaciones', component:  },
+  //{ path: 'OngSafa/Solicitud', component:  },
+  //{ path: 'OngSafa/Perfil/?id', component:  },
 
   { path: '**', component: ErrorPageComponent },
 ];
+const routesDonante: Routes = [
+  // { path: 'OngSafa', redirectTo: 'OngSafa/InicioDonante', pathMatch: 'full' },
 
-const user = 'admin';
+  // { path: 'OngSafa/InicioDonante', component:  },
+  // { path: 'OngSafa/InicioRecibidor', component:  },
+  // { path: 'OngSafa/Donar', component: DonarComponent },
+  // { path: 'OngSafa/Recibir', component: DonarComponent },
+  //{ path: 'OngSafa/Perfil/?id', component:  },
+
+  { path: '**', component: ErrorPageComponent },
+];
+const routesRecibidor: Routes = [
+  // { path: 'OngSafa', redirectTo: 'OngSafa/InicioDRecibidor', pathMatch: 'full' },
+
+  // { path: 'OngSafa/InicioRecibidor', component:  },
+  // { path: 'OngSafa/Recibir', component: DonarComponent },
+  //{ path: 'OngSafa/Perfil/?id', component:  },
+
+  { path: '**', component: ErrorPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
@@ -45,19 +55,22 @@ const user = 'admin';
 })
 export class AppRoutingModule {
   router: any;
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService,public personaTipo: PersonaTipoService) {}
 
   ngOnInit() {
     const user = this.userService.user;
-
+    const persona = this.personaTipo.persona;
     
     let selectedRoutes: Routes;
 
     if (user === 'admin') {
       selectedRoutes = routesAdmin;
-    } else if (user === 'Donante') {
-      selectedRoutes = routesDonanteRecibidor;
-    } else {
+    }
+    if (persona === 'Donante') {
+      selectedRoutes = routesDonante;
+    } else if (persona === 'Recibidor') {
+      selectedRoutes = routesRecibidor;
+    } else { 
       selectedRoutes = routes;
     }
 
