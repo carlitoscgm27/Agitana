b370e9a47d36827f289db054dbafbeea7983b3f3
Antonio López <tonidiez17@gmail.com>
2023-10-04 20:31:35 +0200

    Cambio del navbar depende del usuario

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -5,5 +5,20 @@ import { Injectable } from '@angular/core';
 })
 export class UserService {
 
-  user: string = '';
+  user: string = 'Admin';
+
+  constructor() { }
+
+  // Método para obtener el usuario actual
+  getCurrentUser(): boolean {
+    
+    if(this.user=='Admin'){
+      console.log("entro en getCurrentUser")
+      return true;
+    }else if(this.user=='User'){
+      console.log("entro en getCurrentUser")
+      return true;
+    }else
+    return false;
+  }
 }
